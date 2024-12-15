cf0f10a26f1a518b9b9a5011533799157f8d3811
OzMashiah <ozmashiah10@gmail.com>
2024-12-15 18:07:31 +0200

    added flush=True to see the print output in logs, exercise1.01 done

--- part1/exercise01/logoutput.py
+++ part1/exercise01/logoutput.py
@@ -9,7 +9,7 @@ import random
 def datehash(randomstring):
     # This function gets the date and a random string and prints it.
     date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # current date formatted to string.
-    print(date + ": " + randomstring)
+    print(date + ": " + randomstring, flush=True)
 
 if __name__ == "__main__":
     randomstring = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8)) # 8 length randomly generated string.
