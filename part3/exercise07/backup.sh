aba14c43d54c6a375f967c9d2cd3ad530d96711f
OzMashiah <ozmashiah10@gmail.com>
2024-12-27 20:56:57 +0200

    exercise 3.07 done

--- part3/exercise07/backup.sh
+++ part3/exercise07/backup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Set the backup filename (current date/time)
 BACKUP_FILE="/tmp/backup_$(date +'%Y%m%d%H%M').sql.gz"
 
-# Dump the PostgreSQL database and compress the output
 pg_dump -h $PGHOST -U $PGUSER -d $PGDATABASE | gzip > $BACKUP_FILE
 
-# Upload the backup to Google Cloud Storage
-gsutil cp $BACKUP_FILE gs://your-backup-bucket/
+gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+gsutil cp $BACKUP_FILE gs://devopswithkubernetes-backup-bucket/
 
-# Clean up the backup file
 rm $BACKUP_FILE
