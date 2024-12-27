6e243f6f6a5ccc603b46e412bb77d12ec3aa5166
OzMashiah <ozmashiah10@gmail.com>
2024-12-27 21:05:25 +0200

    exercise 3.07 done

--- part3/exercise07/backup.sh
+++ part3/exercise07/backup.sh
@@ -2,9 +2,8 @@
 
 BACKUP_FILE="/tmp/backup_$(date +'%Y%m%d%H%M').sql.gz"
 
-pg_dump -h $PGHOST -U $PGUSER -d $PGDATABASE | gzip > $BACKUP_FILE
-
 gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+pg_dump -h $PGHOST -U $PGUSER -d $PGDATABASE | gzip > $BACKUP_FILE
 gsutil cp $BACKUP_FILE gs://devopswithkubernetes-backup-bucket/
 
 rm $BACKUP_FILE
